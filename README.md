b8e4f9d92252bdd87903788cd9e69037616da836
Mathieu Barbin <mathieu.barbin@gmail.com>
2025-04-10 10:44:14 +0200

    Initiate dune project from template

---
+++ README.md
@@ -0,0 +1,7 @@
+# crs
+
+[![CI Status](https://github.com/mbarbin/crs/workflows/ci/badge.svg)](https://github.com/mbarbin/crs/actions/workflows/ci.yml)
+
+## Status
+
+This is work in progress, the project is currently an empty template.
---
+++ lib/crs/src/crs.ml
@@ -0,0 +1,31 @@
+(********************************************************************************)
+(*  crs - A tool to parse code review comments embedded in source code          *)
+(*  Copyright (C) 2024-2025 Mathieu Barbin <mathieu.barbin@gmail.com>           *)
+(*                                                                              *)
+(*  This file is part of crs.                                                   *)
+(*                                                                              *)
+(*  crs is free software; you can redistribute it and/or modify it under the    *)
+(*  terms of the GNU Lesser General Public License as published by the Free     *)
+(*  Software Foundation either version 3 of the License, or any later version,  *)
+(*  with the LGPL-3.0 Linking Exception.                                        *)
+(*                                                                              *)
+(*  crs is distributed in the hope that it will be useful, but WITHOUT ANY      *)
+(*  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS   *)
+(*  FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License and     *)
+(*  the file `NOTICE.md` at the root of this repository for more details.       *)
+(*                                                                              *)
+(*  You should have received a copy of the GNU Lesser General Public License    *)
+(*  and the LGPL-3.0 Linking Exception along with this library. If not, see     *)
+(*  <http://www.gnu.org/licenses/> and <https://spdx.org>, respectively.        *)
+(********************************************************************************)
+
+let hello_world = [%sexp "Hello, World!"]
+
+let print_cmd =
+  Command.make
+    ~summary:"print hello world"
+    (let%map_open.Command () = Arg.return () in
+     print_s hello_world)
+;;
+
+let main = Command.group ~summary:"" [ "print", print_cmd ]
