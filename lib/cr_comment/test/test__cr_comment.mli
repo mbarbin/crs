046711ebd6125696d9aaf2d60be92f7c19fcdca5
Mathieu Barbin <mathieu.barbin@gmail.com>
2025-04-12 15:25:36 +0200

    Restructure libraries

--- lib/cr_comment/test/test__cr_comment.mli
+++ lib/cr_comment/test/test__cr_comment.mli
@@ -18,6 +18,3 @@
 (*_  and the LGPL-3.0 Linking Exception along with this library. If not, see     *)
 (*_  <http://www.gnu.org/licenses/> and <https://spdx.org>, respectively.        *)
 (*_*******************************************************************************)
-
-val hello_world : Sexp.t
-val main : unit Command.t
