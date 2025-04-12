206ccb53df53b1162ad34e0b3349020374690988
Mathieu Barbin <mathieu.barbin@gmail.com>
2025-04-12 15:52:29 +0200

    Add headers as per notice

--- lib/cr_comment/src/cr_comment.mli
+++ lib/cr_comment/src/cr_comment.mli
@@ -1,3 +1,46 @@
+(*_*******************************************************************************)
+(*_  crs - A tool to parse code review comments embedded in source code          *)
+(*_  Copyright (C) 2024-2025 Mathieu Barbin <mathieu.barbin@gmail.com>           *)
+(*_                                                                              *)
+(*_  This file is part of crs.                                                   *)
+(*_                                                                              *)
+(*_  crs is free software; you can redistribute it and/or modify it under the    *)
+(*_  terms of the GNU Lesser General Public License as published by the Free     *)
+(*_  Software Foundation either version 3 of the License, or any later version,  *)
+(*_  with the LGPL-3.0 Linking Exception.                                        *)
+(*_                                                                              *)
+(*_  crs is distributed in the hope that it will be useful, but WITHOUT ANY      *)
+(*_  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS   *)
+(*_  FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License and     *)
+(*_  the file `NOTICE.md` at the root of this repository for more details.       *)
+(*_                                                                              *)
+(*_  You should have received a copy of the GNU Lesser General Public License    *)
+(*_  and the LGPL-3.0 Linking Exception along with this library. If not, see     *)
+(*_  <http://www.gnu.org/licenses/> and <https://spdx.org>, respectively.        *)
+(*_*******************************************************************************)
+
+(*_ This module is derived from Iron (v0.9.114.44+47), file
+  * [./hg/cr_comment.mli], which is released under Apache 2.0:
+  *
+  * Copyright (c) 2016-2017 Jane Street Group, LLC <opensource-contacts@janestreet.com>
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  * use this file except in compliance with the License. You may obtain a copy
+  * of the License at:
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+  * License for the specific language governing permissions and limitations
+  * under the License.
+  *
+  * See the file `NOTICE.md` at the root of this repository for more details.
+  *
+  * Changes: ...
+*)
+
 open! Core
 open! Async
 open! Import
