d67ef73093359941c38e45af1b2d8fd1c8007678
Mathieu Barbin <mathieu.barbin@gmail.com>
2025-12-22 09:59:52 +0100

    Add stdlib test lib

---
+++ test/stdlib/test__expect.ml
@@ -0,0 +1,44 @@
+(********************************************************************************)
+(*  crs - A tool for managing code review comments embedded in source code      *)
+(*  Copyright (C) 2024-2025 Mathieu Barbin <mathieu.barbin@gmail.com>           *)
+(*                                                                              *)
+(*  This file is part of crs.                                                   *)
+(*                                                                              *)
+(*  crs is free software; you can redistribute it and/or modify it under the    *)
+(*  terms of the GNU Lesser General Public License as published by the Free     *)
+(*  Software Foundation either version 3 of the License, or any later version,  *)
+(*  with the LGPL-3.0 Linking Exception.                                        *)
+(*                                                                              *)
+(*  crs is distributed in the hope that it will be useful, but WITHOUT ANY      *)
+(*  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS   *)
+(*  FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License and     *)
+(*  the file `NOTICE.md` at the root of this repository for more details.       *)
+(*                                                                              *)
+(*  You should have received a copy of the GNU Lesser General Public License    *)
+(*  and the LGPL-3.0 Linking Exception along with this library. If not, see     *)
+(*  <http://www.gnu.org/licenses/> and <https://spdx.org>, respectively.        *)
+(********************************************************************************)
+
+let%expect_test "require_does_raise did not raise" =
+  (match require_does_raise ignore with
+   | () -> assert false
+   | exception exn -> print_string (Printexc.to_string exn));
+  [%expect {| ("Did not raise.", {}) |}];
+  ()
+;;
+
+let%expect_test "require_equal not equal" =
+  (match require_equal (module Int) 0 42 with
+   | () -> assert false
+   | exception exn -> print_string (Printexc.to_string exn));
+  [%expect {| ("Values are not equal.", { v1 = 0; v2 = 42 }) |}];
+  ()
+;;
+
+let%expect_test "require_not_equal equal" =
+  (match require_not_equal (module Int) 0 0 with
+   | () -> assert false
+   | exception exn -> print_string (Printexc.to_string exn));
+  [%expect {| ("Values are  equal.", { v1 = 0; v2 = 0 }) |}];
+  ()
+;;
